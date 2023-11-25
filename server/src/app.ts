4a541531ed4c6fadc058e5d306daf6bcd1360362
Lex van Os <vanoslex7@gmail.com>
2023-11-25 07:49:21 +0100

    Set-up base MERN project server structure

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+// src/app.ts
+import express from "express";
+import sampleRoute from "./routes/sampleRoute";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use("/api", sampleRoute);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
